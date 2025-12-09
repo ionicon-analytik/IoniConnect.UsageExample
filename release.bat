fb8b7ab38f78d2e45dc43e1ac710fc4b51f396da
Moritz Koenemann <moritz.koenemann@ionicon.com>
2025-12-09 14:08:13 +0100

    Ditch release script.

--- release.bat
+++
@@ -1,4 +0,0 @@
-set ZIPPER=C:\Program Files\7-Zip\7z.exe
-set ARCHIVE=Z:\Ionicon\Projekte\AME2\RELEASE\UsageExample\IoniConnect.UsageExample.zip
-
-"%zipper%" a -tzip "%archive%" -- Program.cs Dependencies\*.nupkg *.sln
